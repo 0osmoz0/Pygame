c0ab7c14157b630424da6b2b7d3f044845e3e357
Mathis Lebel <mathislebel50@gmail.com>
2026-01-29 10:26:28 +0100

    add the button and player_1 with gravity

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 import pygame 
 
+
+# initialiser le projet pygame
 pygame.init()
 screen = pygame.display.set_mode((800, 600), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True 
 
+# couleur du jeu
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+#caracteristique 
+speed = [1, 1]
+gravity = 0.1
+
+# button quitter le jeu 
+button_rect = pygame.Rect(0, 0, 50, 25)
+font = pygame.font.Font(None, 13)
+button_text = font.render("Quitter", True, WHITE)
+
+# Player 1
+Player_1 = pygame.Rect(400, 300, 50, 50)
+
+
 while running:
+    # event pour quitter le jeu soit CMD + Q soit le button quitter 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if button_rect.collidepoint(event.pos):
+                running = False
+            
+    # faire disparaitre l'écrans 
+    screen.fill((0, 0, 0))
 
-    screen.fill("black")
+    speed[1] += gravity
+    Player_1 = Player_1.move(speed)
 
+    # Afficher le boutton sur l'écrans 
+    pygame.draw.rect(screen, RED, button_rect)
+    screen.blit(button_text, (button_rect.x + 10, button_rect.y + 10))
+
+    #affiche le Player sur l'écrans 
+    pygame.draw.rect(screen, WHITE, Player_1)
+
+    #mise a jour de l'écrans 
     pygame.display.flip()
 
+    #limiter le jeu a 60fps 
     clock.tick(60)
 
 pygame.quit 
